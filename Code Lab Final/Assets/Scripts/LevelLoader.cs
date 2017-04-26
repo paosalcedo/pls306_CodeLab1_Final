fa9a41b0f28eda4357aceb7ba30bdb587c00cfbe
Unknown <paosalcedo@gmail.com>
2017-04-26 02:57:09 -0400

    LevelLoader, Raycast reflection.

--- Code Lab Final/Assets/Scripts/LevelLoader.cs
+++ Code Lab Final/Assets/Scripts/LevelLoader.cs
@@ -6,15 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour {
 
-	GameObject enemy;
-	GameObject[] enemies;
-	public float offsetX = 0;
+  	public float offsetX = 0;
 	public float offsetZ = 0;
-	public float coinOffsetY;
-	public float lightOffsetY;
-	
-	public float tileZ;
-
+	 
 	public string[] fileNames;
 	public static int levelNum = 0;
 
@@ -39,7 +33,7 @@ public class LevelLoader : MonoBehaviour {
 
 		GameObject levelHolder = new GameObject ("Level Holder");
 
-		int zPos = 0;
+//		int zPos = 0;
 		int yPos = 0;
 
 		//Read from level text files.
@@ -49,15 +43,15 @@ public class LevelLoader : MonoBehaviour {
 			for (int xPos = 0; xPos < line.Length; xPos++) {
 
 				if (line [xPos] == 'x') {
-					GameObject wall = Instantiate (Resources.Load ("Prefabs/Cube01") as GameObject);
+					GameObject wall = Instantiate (Resources.Load ("Prefabs/Wall") as GameObject);
 
 					wall.transform.parent = levelHolder.transform;
 
 					wall.transform.position = new Vector3 (
 						xPos + offsetX, 
 						yPos,
-						zPos + offsetZ 
-						);
+						0						
+					);
 				}
 			
 				//Instantiate Player
@@ -66,112 +60,17 @@ public class LevelLoader : MonoBehaviour {
 					player.transform.position = new Vector3 (
 						xPos + offsetX, 
 						yPos,
-						zPos + offsetZ
-						);
-				}
-
-				//instantiate platforms
-				if (line [xPos] == 'i') { 
-					GameObject platform = Instantiate (Resources.Load ("Prefabs/Platform") as GameObject);
-					platform.transform.parent = levelHolder.transform;
-					platform.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos,
-						zPos + offsetZ
+						0		
 					);
-				}
-
-				//Instantiate lights with platform.
-				if (line [xPos] == '*') {
-					GameObject platform = Instantiate (Resources.Load ("Prefabs/Platform") as GameObject);
-					platform.transform.parent = levelHolder.transform;
-					platform.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos,
-						zPos + offsetZ
-					);
-
-					GameObject light = Instantiate (Resources.Load ("Prefabs/Light") as GameObject);
-					light.transform.parent = levelHolder.transform;
-					light.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos + lightOffsetY,
-						zPos + offsetZ
-					);
-				}
-
-				if (line [xPos] == '^') {
-					GameObject light = Instantiate (Resources.Load ("Prefabs/Light") as GameObject);
-					light.transform.parent = levelHolder.transform;
-					light.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos + lightOffsetY,
-						zPos + offsetZ
-					);
-				}
-				
-
-				//Instantiate coins
-				if (line [xPos] == 'c') {
-					GameObject platform = Instantiate (Resources.Load ("Prefabs/Platform") as GameObject);
-					platform.transform.parent = levelHolder.transform;
-					platform.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos,
-						zPos + offsetZ
-					);
-
-					GameObject light = Instantiate (Resources.Load ("Prefabs/Light") as GameObject);
-					light.transform.parent = levelHolder.transform;
-					light.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos + lightOffsetY,
-						zPos + offsetZ
-					);
-
-					GameObject coin = Instantiate (Resources.Load ("Prefabs/Coin") as GameObject);
-					coin.transform.parent = levelHolder.transform;
-					coin.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos + coinOffsetY,
-						zPos + offsetZ
-					);
-				}
-
-				//Instantiate goal area.
-				if (line [xPos] == 'g') { 
-					GameObject goal = Instantiate (Resources.Load ("Prefabs/Goal") as GameObject);
-					goal.transform.parent = levelHolder.transform;
-					goal.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos,
-						zPos + offsetZ
-					);
-
-					GameObject light = Instantiate (Resources.Load ("Prefabs/Light") as GameObject);
-					light.transform.parent = levelHolder.transform;
-					light.transform.position = new Vector3 (
-						xPos + offsetX, 
-						yPos + lightOffsetY,
-						zPos + offsetZ
-					);
-				}
-
-				if (line [xPos] == '|') { 
-					yPos -= 4;
-					offsetZ += 17;
 				}
 
 			}
 			
-			zPos--;
+			yPos--;
 		}
 
 		sr.Close();
 	}
-
-
-	float timeAtEndOfLevel;
 
 	// Update is called once per frame
 	void Update () {
@@ -181,9 +80,7 @@ public class LevelLoader : MonoBehaviour {
 	void LoadNextLevel ()
 	{
 		levelNum++;
-		ScoreKeeper.instance.Score = 0;
-		SceneManager.LoadScene ("first");
-
+ 		SceneManager.LoadScene ("main");
 	}
 
 //
---
+++ Code Lab Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	[SerializeField]float speed;
+	[SerializeField]float rotationSpeed;
+	[SerializeField]KeyCode forwardKey;
+	[SerializeField]KeyCode backKey;
+	[SerializeField]KeyCode rotateClockwise;
+	[SerializeField]KeyCode rotateCounterClockwise;
+	
+	Vector3 newVec;
+	Vector3 newDir;
+	[SerializeField]float raycastRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move(transform.right, forwardKey);
+//		Move(-transform.right, backKey);
+		RotatePlayer(1, rotateClockwise);
+		RotatePlayer(-1, rotateCounterClockwise);
+
+		ShootRay();
+	}
+
+	void Move(Vector3 dir, KeyCode key){
+		//if the key passed to this function was pressed
+		if(Input.GetKey(key)){
+			transform.position += dir * speed * Time.deltaTime;
+		}
+	}
+
+	void RotatePlayer(float rotDir, KeyCode key){
+		if(Input.GetKey(key)){
+			transform.Rotate(Vector3.forward * rotDir * rotationSpeed * Time.deltaTime);
+		}
+	}
+
+	void ShootRay ()
+	{
+		Ray ray = new Ray (transform.position, transform.right);
+		Debug.DrawRay (ray.origin, ray.direction * raycastRange, Color.red);
+		
+		RaycastHit rayHit = new RaycastHit ();
+
+		if (Physics.Raycast (ray, out rayHit, raycastRange)) {
+//			Debug.DrawRay (rayHit.point, rayHit.normal*raycastRange, Color.blue);
+			Vector3 reflection = Vector3.Reflect(ray.direction, rayHit.normal);
+ 			Debug.DrawRay(rayHit.point, reflection * raycastRange, Color.blue);
+		}
+	}
+
+}
